e48fe15bbea1498aa372b7a6c2bcccf89c8c8da0
Rushikesh Raskar <raskar.rushikesh4@gmail.com>
2018-09-09 19:05:44 +0530

    Database Test code in login.aspx.cs and data model created for user

---
+++ Hostel Management System Rushi/App_Code/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class User
+{
+    private String _id;
+    private String _uname;
+    private String _pword;
+    public User()
+    { 
+
+    }
+    public User(String id, String uname,String pword)
+    {
+        _id = id;
+        _uname = uname;
+        _pword = pword;
+    }
+
+    public String Id
+    {
+        get { return _id; }
+        set { _id = value; }
+    }
+    public String Uname
+    {
+        get { return _uname; }
+        set { _uname = value; }
+    }
+    public String Pword
+    {
+        get { return _pword; }
+        set { _pword = value; }
+    }
+    public String getData()
+    {
+        return Id + " " + Uname + " " + Pword;
+    }
+}
--- Hostel Management System Rushi/src/Login.aspx.cs
+++ Hostel Management System Rushi/src/Login.aspx.cs
@@ -13,6 +13,25 @@ public partial class src_Login : System.Web.UI.Page
     }
     protected void _Login(object sender, EventArgs e)
     {
-        ScriptManager.RegisterClientScriptBlock(this,this.GetType(),"Alert Message", "alert('Login button is clicked')",true);
+        String uname = String.Format("{0}",Request.Form["uname"]);
+        String pword = String.Format("{0}", Request.Form["pword"]);
+    //ScriptManager.RegisterClientScriptBlock(this,this.GetType(),"Alert Message", "alert('Login Successful')", true);
+
+
+           String connectionString= "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = " + "D:\\workSpaces\\studio2017\\repos\\Hostel Management System Rushi\\Hostel Management System Rushi\\App_Data\\Database.mdf" + "; Integrated Security = True";
+            System.Data.SqlClient.SqlConnection con = new System.Data.SqlClient.SqlConnection(connectionString);
+        con.Open();
+        Console.WriteLine("connection opened successfully");
+        System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand("select * from Users",con);
+        User[] user = new User[5];
+            int i=0;
+        System.Data.SqlClient.SqlDataReader reader = cmd.ExecuteReader();
+        while(reader.Read()){
+                user[i++] = new User(reader["Id"].ToString(), (String)reader["uname"], (String)reader["pword"]);
+                }
+
+          con.Close();
+            Console.WriteLine("connection closed successfully");
+        ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "Alert Message", "alert('"+user[0].getData()+"')", true);
     }
 }
